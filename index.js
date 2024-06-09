d507bd267815eeb128b37776da459d6ec537b4b2
Niloy Das <niloydas5215@gmail.com>
2024-06-09 08:14:10 +0600

    user role updated

--- index.js
+++ index.js
@@ -61,6 +61,14 @@ async function run() {
             res.send(result);
         });
 
+        app.put('/userRole/:id',async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const user = req.body;
+            const updatedUser = { $set: { role: user.role } };
+            const result = await userCollection.updateOne(query, updatedUser);
+            res.send(result);
+        })
 
         // Properties related api
         app.post('/properties', async (req, res) => {
